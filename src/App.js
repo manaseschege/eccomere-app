98c6f2c88cdd3553672bf011017c73c8afe81e9a
Manases Chege <manaseschege0@gmail.com>
2023-03-08 17:28:31 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Header from "Components/Header"
+import {Routes,Route} from "react-router-dom"
+import Cart from "Pages/Cart"
+import Photos from "Pages/Photos"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+  return(
+    <div>
+    <Header/>
+    <h1>Home Page</h1>
+    <Routes>
+      <Route exact path="/" element={<Photos/>}/>
+      <Route path="/Cart" element={<Cart/>}/>
+    </Routes>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
